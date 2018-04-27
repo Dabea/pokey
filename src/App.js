9a66837bedb7efda4761484d8062259bd1553b3e
Abraham Anderson <abeis@msn.com>
2018-04-27 17:42:25 -0500

    setup classes that take objects

--- src/App.js
+++ src/App.js
@@ -3,9 +3,23 @@ import logo from './logo.svg';
 import './App.css';
 
 
+class Pokemon extends Component {
+   styles = {};
+  render(){
+   return( 
+   <div className="pokemon">
+      <h1>Pokemon:{this.props.pokemon.name}</h1>
+      <h2>Type:{this.props.pokemon.type}</h2>
+   </div>)
+  }
+
+}
+
 
 class App extends Component {
   render() {
+    
+    const monster = [{name:'Bulbasaur', type:'Grass'}, {name:'Charmander', type:'Fire'}];
     return (
       <div className="App">
         <header className="App-header">
@@ -14,6 +28,10 @@ class App extends Component {
         </header>
         <p className="App-intro">
         </p>
+        <div className="center">
+          <Pokemon pokemon={monster[0]} />
+          <Pokemon  pokemon={monster[1]} />
+        </div>
        
       </div>
     );
